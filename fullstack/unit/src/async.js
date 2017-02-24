2eebce879e4a4a94e5f88d29f6a5608eda6a18d1
James Gu <jamesgu521@gmail.com>
2017-02-23 19:18:54 -0800

    Finish async and count

--- fullstack/unit/src/async.js
+++ fullstack/unit/src/async.js
@@ -1,7 +1,18 @@
 module.exports = {
-    async : function(value) {
+    async : (value) => {
+      return new Promise((resolve, reject) => {
+        resolve(value);
+      })
     },
 
-    manipulateRemoteData : function(url) {
+    manipulateRemoteData : (url) => {
+      return new Promise((resolve, reject) => {
+        fetch(url)
+          .then(response => response.json())
+          .then(json => {
+            let result = json.people.map((person) => person.name).sort();
+            resolve(result);
+          });
+      });
     }
 };
--- fullstack/unit/src/count.js
+++ fullstack/unit/src/count.js
@@ -1,6 +1,22 @@
 
 module.exports = {
     count : function (start, end) {
+      let i = start;
+      let intervalID = setInterval(function () {
+        console.log(i);
+        if (++i > end) {
+          clearInterval(intervalID);
+        }
+      }, 100);
+      
+      // give reference to intervalID through closure
+      let cancel = function () {
+        clearInterval(intervalID);
+      }
+      return {
+        cancel : function() {
+          cancel(intervalID)
+        }
+      };
     }
 };
-
